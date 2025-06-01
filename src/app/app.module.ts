4524d9f77d50fdfe925565f434a5ccf53f614cda
Wilian Morales <willian.moralesch@gmail.com>
2025-05-31 20:57:17 -0500

    chore: completa actualización Angular 18.2.19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { SharedModule } from '@shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
@@ -8,18 +8,11 @@ import { GifsModule } from './gifs/gifs.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    SharedModule,
-    GifsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        SharedModule,
+        GifsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
