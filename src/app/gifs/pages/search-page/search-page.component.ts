492a9f77bed0f608956529aa7ba0f15d27f88d80
Wilian Morales <willian.moralesch@gmail.com>
2025-07-04 19:47:00 -0500

    feat(search): diseño e implementación de resultados de búsqueda

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { GifsService } from '@service/gifs.service';
+import { Component, inject, signal } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { Gif } from '../../interfaces/gif.interfaces';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+  GifsService = inject(GifsService);
+  gifs = signal<Gif[]>([]);
+
+  onSearch(query: string): void {
+    this.GifsService.searchGifs(query)
+      .subscribe((resp) => {
+        this.gifs.set(resp)
+      })
+  }
+}
